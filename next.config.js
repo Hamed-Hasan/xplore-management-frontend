d1c78a88e5d2a5bb1bc967657af296876161e80b
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-14 13:11:30 +0300

    Refactor TypeScript config for enhanced compatibility 🚀 Update TailwindCSS content paths for better file inclusion 🔄 Enhance theme with new radial gradient background 🌈 Optimize color palette, introducing elegant shade 💅 Configure responsive screen variants for improved layout 📐 Add support for additional image domains in NextConfig 🖼️ Incorporate core web vitals into project for better performance 🔍

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["plus.unsplash.com", "images.unsplash.com", "res.cloudinary.com"],
+  },
+};
+
+module.exports = nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#2a9d8f",
+        secondary: "#457b9d",
+        accent: "#0077b6",
+        elegant: "#f9f9f9",
+      },
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+  important: true,
+  variants: {
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+  },
+};
+
+export default config;
