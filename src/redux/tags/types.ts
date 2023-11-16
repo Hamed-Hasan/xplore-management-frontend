c9233fe0516a22f31393782b06077d618a18de46
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-16 13:33:09 +0300

    🚀 Added enum definition for tag types 🌐 Updated tag types list with all available options 🛠️ Refactored code structure for better organization 📝 Documented tag types and their usage in the code 💡 Introduced constant list for tag types to enhance readability

---
+++ src/redux/tags/types.ts
@@ -0,0 +1,21 @@
+export enum tagTypes {
+  user = "user",
+  services = "services",
+  cart = "cart",
+  booking = "booking",
+  review = "review",
+  feedback = "feedback",
+  blog = "blog",
+  faq = "faq",
+}
+
+export const tagTypesList = [
+  tagTypes.user,
+  tagTypes.services,
+  tagTypes.cart,
+  tagTypes.booking,
+  tagTypes.review,
+  tagTypes.feedback,
+  tagTypes.blog,
+  tagTypes.faq,
+];
