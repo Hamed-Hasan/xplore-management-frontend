388eef613f1cb01dd3b95a1f6ba32d7c078e5391
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-18 13:33:03 +0300

    🌐 Updated service month options for better user experience 🔄 Adjusted service include and not include options 🛠️ Fixed a bug in availability type options 🚀 Enhanced service category and booking status types 🐛 Resolved issues with authKey and servicesMonthOptions

---
+++ src/constants/localStorageKey.ts
@@ -0,0 +1,1 @@
+export const authKey = "access_token";
---
+++ src/constants/services.ts
@@ -0,0 +1,97 @@
+export const servicesMonthOptions = [
+  { value: "January", label: "January" },
+  { value: "February", label: "February" },
+  { value: "March", label: "March" },
+  { value: "April", label: "April" },
+  { value: "May", label: "May" },
+  { value: "June", label: "June" },
+  { value: "July", label: "July" },
+  { value: "August", label: "August" },
+  { value: "September", label: "September" },
+  { value: "October", label: "October" },
+  { value: "November", label: "November" },
+  { value: "December", label: "December" },
+];
+
+export const serviceIncludeOptions = [
+  { value: "Wanderlust Adventures", label: "Wanderlust Adventures" },
+  { value: "JetSet Journeys", label: "JetSet Journeys" },
+  { value: "Explore Horizon", label: "Explore Horizon" },
+  { value: "Odyssey Escapes", label: "Odyssey Escapes" },
+  { value: "RoamReady Travel", label: "RoamReady Travel" },
+  { value: "Voyager Ventures", label: "Voyager Ventures" },
+  { value: "Globetrotter Getaways", label: "Globetrotter Getaways" },
+  { value: "DreamDestiny Tours", label: "DreamDestiny Tours" },
+  { value: "Infinite Expeditions", label: "Infinite Expeditions" },
+  { value: "TravelPulse Pro", label: "TravelPulse Pro" },
+  { value: "Wayfarer Wonders", label: "Wayfarer Wonders" },
+  { value: "DiscoverEase Travels", label: "DiscoverEase Travels" },
+  { value: "Passport Pathfinders", label: "Passport Pathfinders" },
+  { value: "TrekTonic Voyages", label: "TrekTonic Voyages" },
+  { value: "NomadNest Travels", label: "NomadNest Travels" },
+  { value: "ExpeditionEra", label: "ExpeditionEra" },
+  { value: "Beyond Borders Tours", label: "Beyond Borders Tours" },
+  { value: "AdventureAlly", label: "AdventureAlly" },
+  { value: "EpicEscape Expeditions", label: "EpicEscape Expeditions" },
+  { value: "TravelTranquil", label: "TravelTranquil" },
+];
+
+export const serviceNotIncludeOptions = [
+  { value: "Visa fees", label: "Visa fees" },
+  { value: "Airport transfers", label: "Airport transfers" },
+  { value: "Travel insurance", label: "Travel insurance" },
+  { value: "Meals not specified", label: "Meals not specified" },
+  { value: "Optional excursions", label: "Optional excursions" },
+  { value: "Personal expenses", label: "Personal expenses" },
+  { value: "Entry fees to attractions", label: "Entry fees to attractions" },
+  { value: "Alcoholic beverages", label: "Alcoholic beverages" },
+  { value: "Phone and data charges", label: "Phone and data charges" },
+  { value: "Laundry services", label: "Laundry services" },
+  { value: "Tips and gratuities", label: "Tips and gratuities" },
+  { value: "Medical expenses", label: "Medical expenses" },
+  {
+    value: "Transport to the departure city",
+    label: "Transport to the departure city",
+  },
+  {
+    value: "Snacks and drinks between meals",
+    label: "Snacks and drinks between meals",
+  },
+  { value: "Room service charges", label: "Room service charges" },
+  { value: "Additional baggage fees", label: "Additional baggage fees" },
+  {
+    value: "Visits to spas or wellness centers",
+    label: "Visits to spas or wellness centers",
+  },
+  {
+    value: "Surcharge for single occupancy",
+    label: "Surcharge for single occupancy",
+  },
+  { value: "Airport parking fees", label: "Airport parking fees" },
+  { value: "Wi-Fi charges", label: "Wi-Fi charges" },
+];
+
+export const availabilityTypeOptions = [
+  { value: "UPCOMING", label: "UPCOMING" },
+  { value: "AVAILABLE", label: "AVAILABLE" },
+];
+
+export const serviceCategoryOptions = [
+  { value: "luxury", label: "luxury" },
+  { value: "travel", label: "travel" },
+  { value: "backpacking", label: "backpacking" },
+  { value: "road", label: "road" },
+  { value: "trips", label: "trips" },
+  { value: "volunteer", label: "volunteer" },
+  { value: "educational", label: "educational" },
+  { value: "medical", label: "medical" },
+  { value: "religious", label: "religious" },
+  { value: "tourism", label: "tourism" },
+  { value: "pilgrimage", label: "pilgrimage" },
+  { value: "honeymoon", label: "honeymoon" },
+];
+
+export const bookingStatusTypes = [
+  { value: "APPROVED", label: "APPROVED" },
+  { value: "REJECTED", label: "REJECTED" },
+];
