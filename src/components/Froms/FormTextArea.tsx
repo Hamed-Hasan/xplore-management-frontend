604bbf438e1b3dcc9333b432b8d9c41387861585
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-20 14:13:17 +0300

    🚀 Added FormTextArea component with controlled input. 🐛 Fixed styling issue in FormTextArea. 🌟 Enhanced error handling for better user feedback. 📝 Updated README with usage instructions. 🛠️ Refactored code for improved readability and maintainability.

---
+++ src/components/Froms/FormTextArea.tsx
@@ -0,0 +1,51 @@
+import { getErrorMessageByPropertyName } from "@/utils/schema-validator";
+import { Input } from "antd";
+import { Controller, useFormContext } from "react-hook-form";
+
+type TextAreaProps = {
+  name: string;
+  label?: string;
+  rows?: number;
+  value?: string;
+  placeholder?: string;
+};
+
+const FormTextArea = ({
+  name,
+  label,
+  rows,
+  value,
+  placeholder,
+}: TextAreaProps) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  const errorMessage = getErrorMessageByPropertyName(errors, name);
+
+  return (
+    <div className={`flex flex-col  w-full`}>
+      {label ? label : null}
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <Input.TextArea
+            rows={rows}
+            placeholder={placeholder}
+            {...field}
+            defaultValue={value}
+          />
+        )}
+      />
+      <div>
+        <small style={{ color: "red", marginTop: "10px" }}>
+          {errorMessage}
+        </small>
+      </div>
+    </div>
+  );
+};
+
+export default FormTextArea;
